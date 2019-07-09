13e5eb3d751485869ef1239b02cc8e474e9b1c84
Charly <caralopezrom@unal.edu.co>
2019-07-08 22:10:53 -0500

    Cambio ip deploy3

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User
     end
 
     def self.get_user(id_user)
-        response = get("http://34.67.242.29:3001/users/#{id_user}")
+        response = get("http://35.224.121.94:3001/users/#{id_user}")
         if response.success?
             new(response)
         else
